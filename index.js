d4f5cda74f3279c0fc5b9c3980407b99f45fa8c8
Gassan <g_fox_fts@yahoo.com>
2020-05-28 01:26:52 +0200

    (type, model)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class Car {
     }
 }
 
-class carFactory {
+class CarFactory {
     createCar(type) {
         switch(type) {
             case 'civic':
@@ -17,7 +17,37 @@ class carFactory {
     }
 }
 
-const factory = new carFactory();
-const myHonda = factory.createCar('honda');
+class SUV {
+    constructor(doors, engine, color) {
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;   
+    }
+}
 
-console.log(myHonda);
+class SuvFactory {
+    createCar(type) {
+        switch(type) {
+            case 'cx5':
+                return new Car(4, 'V6', 'grey')
+            case 'sante fe':
+                return new Car(2, "V8", 'red')
+        }
+    }
+}
+
+const carFactory = new CarFactory();
+const suvFactory = new SuvFactory();
+
+const autoManufacturer = (type, model) => {
+    switch(type) {
+        case 'car':
+            return carFactory.createCar(model);
+        case 'suv':
+            return suvFactory.createCar(model);
+    }
+}
+
+const cx5 = autoManufacturer('suv', 'cx5');
+
+console.log(cx5);
